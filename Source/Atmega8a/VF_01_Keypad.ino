4aea9ccce5c9735c6aa17e390b7383d978d29e2f
IPRuslan <112751251+IPRuslan@users.noreply.github.com>
2026-01-26 15:20:39 +0200

    Add files via upload

---
+++ Source/Atmega8a/VF_01_Keypad.ino
@@ -0,0 +1,41 @@
+#include <Keypad.h>
+
+// ===== Keypad =====
+const byte ROWS = 4;
+const byte COLS = 4;
+
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3','A'},
+  {'4','5','6','B'},
+  {'7','8','9','C'},
+  {'*','0','#','D'}
+};
+
+byte rowPins[ROWS] = {8, 9, 10, 11};
+byte colPins[COLS] = {4, 5, 6, 7};
+
+Keypad keypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS);
+
+void setup() {
+  Serial.begin(9600);
+
+  // Зменшуємо агресивність keypad
+  keypad.setDebounceTime(25);
+  keypad.setHoldTime(500);
+}
+
+void loop() {
+  char customKey = customKeypad.getKey();
+
+  if (customKey){
+    if(customKey == 'D'){
+      // "Enter"
+      Serial.print("com_r"); 
+    } 
+    else {
+      // кнопки
+      Serial.print(customKey);
+    }
+  }
+}
+
